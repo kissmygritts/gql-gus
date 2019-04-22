e257c320c309c6e7aec4684f6b4d5d7ddc10bc78
kissmygritts <kissmygritts08@gmail.com>
2019-04-22 11:36:02 -0700

    Tests: added simple tests for species

---
+++ src/graphql/species/species.spec.js
@@ -0,0 +1,31 @@
+const { makeExecutableSchema } = require('graphql-tools')
+const { graphql } = require('graphql')
+const { pgp } = require('./../../db')
+const { typeDefs, resolvers } = require('./..')
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+const gqlRunner = (query, variables) => {
+  return graphql(schema, query, null, {}, variables)
+}
+
+const SPECIES_QUERY = /* GraphQL */`
+  query {
+    species {
+      id
+      common_name
+      species_name
+    }
+  }
+`
+
+describe('species', () => {
+  test('SPECIES_QUERY should return data', async () => {
+    const response = await gqlRunner(SPECIES_QUERY, {})
+    expect(response).toHaveProperty('data')
+  })
+
+  test('data.species should be array', async () => {
+    const response = await gqlRunner(SPECIES_QUERY, {})
+    expect(response.data.species).toBeInstanceOf(Array)
+  })
+})
