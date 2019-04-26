a491a0d4a13cf541083c73858d18866837c35cdf
kissmygritts <kissmygritts08@gmail.com>
2019-04-26 11:21:21 -0700

    Add production environment details

--- src/config/index.js
+++ src/config/index.js
@@ -5,5 +5,8 @@ console.log(environment)
 
 exports.environment
 
+// exports.pgsqlUri = environment === 'test' ?
+//   process.env.PGSQL_TEST : process.env.PGSQL_DEV
 exports.pgsqlUri = environment === 'test' ?
-  process.env.PGSQL_TEST : process.env.PGSQL_DEV
+  process.env.PGSQL_TEST : environment === 'production' ?
+  process.env.PGSQL_PROD : process.env.PGSQL_DEV
