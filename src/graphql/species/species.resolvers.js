f9d045f15336f1082004367d97d016a7b05a172d
kissmygritts <kissmygritts08@gmail.com>
2019-07-17 14:38:43 -0700

    Refactor: include missing fields from species query

--- src/graphql/species/species.resolvers.js
+++ src/graphql/species/species.resolvers.js
@@ -5,7 +5,21 @@ module.exports = {
   Query: {
     species: async (parent, args, context, info) => {
       const { limit, filter } = args
-      const sql = 'select id, common_name, species_name, subspecies from species'
+      const sql = `
+        select 
+          id,
+          common_name,
+          species_name,
+          t_phylum,
+          t_class,
+          t_order,
+          t_family,
+          genus,
+          species,
+          subspecies,
+          species_group
+        from species
+      `
       
       return db.many('$/sql:raw/ $/where:raw/ $/pagination:raw/', {
         sql,
