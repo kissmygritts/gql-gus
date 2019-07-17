3ed36bc1e850ea4f0997b46e6cd0da582d37488c
kissmygritts <kissmygritts08@gmail.com>
2019-07-17 11:18:48 -0700

    Refactor: include filter and pagination implementation to species query

--- src/graphql/species/species.resolvers.js
+++ src/graphql/species/species.resolvers.js
@@ -1,54 +1,17 @@
-const { db, pgp } = require('./../../db')
-
-const operators = {
-  eq: '=',
-  gte: '>=',
-  gt: '>',
-  lte: '<=',
-  lt: '<',
-  neq: '!=',
-  in: 'IN',
-  like: '~~'
-}
-
-const getOperator = clauseObj => operators[Object.keys(clauseObj)[0]]
-const getCriteria = clauseObj => Object.values(clauseObj)[0]
-
-const parseFilter = filter => Object.keys(filter)
-  .map(field => ({
-      field,
-      operator: getOperator(filter[field]),
-      criteria: getCriteria(filter[field])
-    }))
-
-const sqlizeFilter = criteriaArray => criteriaArray
-  .map(m => pgp.as.format('$/field:name/ $/operator:raw/ $/criteria/', m))
-  .reduce((acc, curr) => acc + ' AND ' + curr)
-
-const filterToWhere = filter => sqlizeFilter(parseFilter(filter))
+const { db } = require('./../../db')
+const { offsetPagination, sqlizeFilter } = require('./../../util')
 
 module.exports = {
   Query: {
     species: async (parent, args, context, info) => {
-      let sql
-      let limit
-
-      // where filter logic
-      if (args.filter) {
-        sql = pgp.as.format('select id, common_name, species_name, subspecies from species where $1:raw',
-                                filterToWhere(args.filter))
-      } else {
-        sql = 'select id, common_name, species_name, subspecies from species'
-      }
-
-      // limit logic
-      if (args.limit) {
-        limit = pgp.as.format('limit $/first/', args.limit)
-        sql = sql + ' ' + limit
-      }
-
-      // execute query
-      return db.many(sql)
+      const { limit, filter } = args
+      const sql = 'select id, common_name, species_name, subspecies from species'
+      
+      return db.many('$/sql:raw/ $/where:raw/ $/pagination:raw/', {
+        sql,
+        where: sqlizeFilter(filter),
+        pagination: offsetPagination(limit)
+      })
     }
   }
 }
