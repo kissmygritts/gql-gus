a583b52ddb079686ff6dd92a47994268a889ab15
kissmygritts <kissmygritts08@gmail.com>
2019-09-03 16:45:44 -0700

    use .sql file instead of inline query

--- src/db/repos/animal-encounters.js
+++ src/db/repos/animal-encounters.js
@@ -1,4 +1,5 @@
 const { Repo } = require('./../../util/repo-factory')
+const sql = require('./../sql').animalEncounters
 const { sqlizeFilter, offsetPagination } = require('./../../util/pgsql-helpers')
 
 const fields = [
@@ -17,32 +18,10 @@ const fields = [
 const table = 'animal_encounters'
 
 const extend = repo => ({
-  all: args => {
-    const sql = `
-      select
-        encounters.id as id,
-        animals.id as animal_id,
-        species.id as species_id,
-        species.common_name,
-        species.species_name,
-        animals.ind_id,
-        encounters.life_status,
-        encounters.age_class,
-        encounters.sex,
-        encounters.n,
-        encounters.reencounter,
-        encounters.relocation
-      from animals
-        right join encounters on animals.id = encounters.animal_id
-        left join species on encounters.species_id = species.id
-      $/where:raw/
-      $/pagination:raw/
-    `
-    return repo.pgp.as.format(sql, {
-      where: sqlizeFilter(args.filter),
-      pagination: offsetPagination(args.limit)
-    })
-  }
+  select: ({ filter, limit }) => repo.pgp.as.format(sql.all, {
+    where: sqlizeFilter(filter),
+    pagination: offsetPagination(limit)
+  })
 })
 
 const AnimalEncounterRepo = Repo({ fields, table })({ extend })
--- src/services/animal-encounters.js
+++ src/services/animal-encounters.js
@@ -2,9 +2,5 @@ const { db } = require('./../db')
 const { AnimalEncounters } = require('./../db/repos')
 
 module.exports = {
-  all: (args) => {
-    console.log(AnimalEncounters)
-    const query = AnimalEncounters.all(args)
-    return db.any(query)
-  }
+  all: (args) => db.any(AnimalEncounters.select(args))
 }
