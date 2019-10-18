3220ca77e1540c201755a8b468095dc10533f883
kissmygritts <kissmygritts08@gmail.com>
2019-10-17 17:16:09 -0700

    Refactor: delete unused code

--- src/graphql/observation-feed/observations-feed.resolvers.js
+++ src/graphql/observation-feed/observations-feed.resolvers.js
@@ -1,26 +1,6 @@
 const observationFeed = require('./../../services/observation-feed')
 
 module.exports = {
-  // Query: {
-  //   getObservationFeed: async (parent, args, context, info) => {
-  //     const { limit, filter } = args
-
-  //     return db.manyOrNone('$/sql:raw/ $/where:raw/ $/pagination:raw/', {
-  //       sql: 'select * from wildlife_events_feed',
-  //       where: sqlizeFilter(filter),
-  //       pagination: offsetPagination(limit)
-  //     })
-  //   }
-  // },
-
-  // WildlifeEvent: {
-  //   wildlife_encounters: (parent, args, context, info) => {
-  //     return parent.wildlife_encounters
-  //   },
-  //   wildlife_abundance: (parent, args, context, info) => {
-  //     return parent.wildlife_abundance
-  //   }
-  // }
   Query: {
     getObservationFeed: async (parent, args, context, info) => observationFeed.all(args)
   },
